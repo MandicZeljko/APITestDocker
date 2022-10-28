da5f9120f9667f06467237b91b75c37208201005
Zeljko Mandic <zeljko_va@yahoo.ca>
2022-10-28 10:39:52 +0200

    video=false

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: false,
  
   e2e: {
     setupNodeEvents(on, config) {
+
+      video: false
       
       // implement node event listeners here
     },
