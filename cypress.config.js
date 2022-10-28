0211048117b275914997c98aca37af8ebb95ceeb
Zeljko Mandic <zeljko_va@yahoo.ca>
2022-10-28 10:26:20 +0200

    test file upload

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  
+ 
   e2e: {
     setupNodeEvents(on, config) {
-      reporter: 'mochawesome'
+      
       // implement node event listeners here
     },
   },
